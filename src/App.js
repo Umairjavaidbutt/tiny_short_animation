b5e40c7d16eb778f13373f3ee3d608132d0c2e3b
unknown <umair.javaid.butt@gmail.com>
2020-08-03 20:26:24 +0500

    upload

--- src/App.js
+++ src/App.js
@@ -1,24 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import { Tween } from 'react-gsap';
+
 
 function App() {
+  const [navlist, setnavlist] = useState(true)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <body>
+        <header>
+          <nav className={navlist ? "main-nav" : "open"}>
+          <Tween
+              from={{ x: '-200px', opacity:0 }}
+              to={{ x: 0, opacity: 1 }}
+              duration={2}
+              delay={.5}
+            >
+            <div className="logo">
+              <a href="/">LHR</a>
+            </div>
+            </Tween>
+            <Tween
+              from={{ x: '-200px', opacity:0 }}
+              to={{ x: 0, opacity: 1 }}
+              duration={2}
+              delay={.8}
+            >
+            <div className="hamburger-menu" onClick={()=> setnavlist(!navlist)}>
+              <span className="bar">
+              </span>
+            </div>
+            </Tween>
+            <ul className="nav-list">
+              <li className="nav-item">
+                <a href="/" className="nav-link">Home</a>
+              </li>
+              <li className="nav-item">
+                <a href="/" className="nav-link">About</a>
+              </li>
+              <li className="nav-item">
+                <a href="/" className="nav-link">Contact</a>
+              </li>
+              <li className="nav-item">
+                <a href="/" className="nav-link">Map</a>
+              </li>
+            </ul>
+          </nav>
+        </header>
+        <section className="hero">
+            <Tween
+              from={{ scaleX: 0 }}
+              to={{ scaleX: 1 }}
+              duration={1}
+            >
+          <div className="hero-clipped">
+          <Tween
+              from={{ yPercent: 40 , opacity:0 }}
+              to={{ yPercent: -50, opacity: 1 }}
+              duration={1}
+              delay={1.3}
+            >
+             <div className="hero-text">
+              <h1 className="main-heading">Lahore</h1>
+              <h2 className="sub-heading">Learn about city attractions</h2>
+              <h6>Responsive to screen size</h6>
+              <a href="/" className="cta-btn">Virtual Tour</a>
+            </div>
+            </Tween>
+          </div>
+          </Tween>
+        </section>
+      </body>
     </div>
   );
 }
